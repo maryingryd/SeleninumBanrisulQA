37c8bff07dea9d025c564eca7fcf251a60547861
Ingryd Mary <101146185+maryingryd@users.noreply.github.com>
2023-03-24 15:05:34 -0300

    day 2.2

--- demo/src/test/java/Tasks/LoginTask.java
+++ demo/src/test/java/Tasks/LoginTask.java
@@ -1,20 +1,25 @@
 package Tasks;
 
+
 import org.openqa.selenium.WebDriver;
 
 import PageObjects.LoginPage;
+import Validations.LoginValidations;
 
 public class LoginTask {
     private WebDriver driver;
     private LoginPage loginPage;
-
+    private LoginValidations loginValidation;
+   
     public LoginTask(WebDriver driver) {       
         this.driver = driver;
-        loginPage = new LoginPage(driver);
+        loginPage = new LoginPage(this.driver);
+        loginValidation = new LoginValidations(this.driver);       
     }
     // metodo da interação (procedural)
 
     public void efetuarlogin() {
+        loginValidation.validationLoginPage();
         loginPage.getUserNameInput().sendKeys("standard_user");
         loginPage.getPasswordInput().sendKeys("secret_sauce");
         loginPage.getloginButton().click();
--- demo/src/test/java/TestCases/RealizarCompraComSucessoTest.java
+++ demo/src/test/java/TestCases/RealizarCompraComSucessoTest.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.WebDriver;
 
 import Framework.TestBase;
 import Tasks.LoginTask;
+import Tasks.ProdutoTask;
 //Aqui herda Test Base, pois testcases, não tem construtor e this.driver;
 public class RealizarCompraComSucessoTest extends TestBase {
     private WebDriver driver = getDriverManager();
     LoginTask loginTask = new LoginTask (driver);
+    ProdutoTask produtoTask = new ProdutoTask(driver);
+    
     
     @Test
     public void realizarCompra () {
         loginTask.efetuarlogin();
+        produtoTask.selecionarProduto();
     }
       
 
